b9ac8a49253a03093ba20a39265a2eb2fa327d2c
Jared Porter <jaredp17@me.com>
2020-07-28 18:36:57 -0400

    Put route working to add exercises.

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,29 @@ const router = require("express").Router();
 const db = require("../models");
 
 // Initialize new workout
-router.post("/api/workouts", ({body}, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
     .then((dbWorkout) => {
-      console.log("Success!")
       res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+// Add exercises to workouts
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: { exercises: body },
+    },
+    {
+      new: true,
+    }
+  )
+    .then((addExercise) => {
+      res.json(addExercise);
     })
     .catch((err) => {
       res.status(500).json(err);
